8e0fb0fe52c25c4571b7aee33026bceecfea9b83
Arpit Singh <asr827013@gmail.com>
2026-03-14 14:21:15 +0530

    Added deployed routes

--- Backend/app.py
+++ Backend/app.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://medfed.onrender.com"
+        "https://medfed.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
